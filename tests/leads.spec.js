798ed66c6d3730133ab46b581511d9299e4525e4
Matheus Koyama <koyamaCRS@gmail.com>
2026-06-01 11:15:38 -0300

    checkpoint e buscas de elementos

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -1,10 +1,21 @@
 // @ts-check
-import { test } from '@playwright/test';
+import { test, expect} from '@playwright/test';
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   //await page.click('//button[text()="Aperte o play... se tiver coragem"]')  
   await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+
+  //checkpoint verificar se esta na tela certa
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+   ).toHaveText('Fila de espera')
+
+  await page.locator('input[name="name"]').fill('Matheus')
+  await page.getByPlaceholder('Seu email principal').fill('qalab@hotmail.com')
+
+ 
   await page.waitForTimeout(3000);
+
 });
